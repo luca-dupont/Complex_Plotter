68f6db11d88aeb16f19fa598dae558cd2b63e5cb
lucaskye <1902060@brebeuf.qc.ca>
2024-04-27 23:20:51 -0400

    Do everytann

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+extern crate num_complex;
 use macroquad::prelude::*;
+use num_complex::Complex;
+use std::f64::consts::PI;
 
 const WIDTH: f32 = 750.0;
 const HEIGHT: f32 = 750.0;
@@ -14,6 +17,32 @@ fn map_value(value: f32, from_min: f32, from_max: f32, to_min: f32, to_max: f32)
     normalized_value * (to_max - to_min) + to_min
 }
 
+fn angle_to_rgb(hue: f32) -> (f32, f32, f32) {
+    let chroma = 1.0;
+    let x = chroma * (1.0 - ((hue / 60.0) % 2.0 - 1.0).abs());
+    let m = 1.0 - chroma / 2.0;
+
+    let (r, g, b) = if hue < 60.0 {
+        (chroma, x, 0.0)
+    } else if hue < 120.0 {
+        (x, chroma, 0.0)
+    } else if hue < 180.0 {
+        (0.0, chroma, x)
+    } else if hue < 240.0 {
+        (0.0, x, chroma)
+    } else if hue < 300.0 {
+        (x, 0.0, chroma)
+    } else {
+        (chroma, 0.0, x)
+    };
+
+    let r = r + m;
+    let g = g + m;
+    let b = b + m;
+
+    (r, g, b)
+}
+
 #[macroquad::main("Complex function grapher")]
 async fn main() {
     request_new_screen_size(WIDTH / W_OS_FACTOR, HEIGHT / H_OS_FACTOR);
@@ -22,7 +51,7 @@ async fn main() {
 
     let texture = Texture2D::from_image(&image);
 
-    println!("{:?}", (image.width(), image.height()));
+    let mut boundary = 100.;
 
     loop {
         let w = image.width();
@@ -30,17 +59,47 @@ async fn main() {
 
         clear_background(BLACK);
 
+        if is_key_down(KeyCode::Up) {
+            boundary /= 1.1;
+        } else if is_key_down(KeyCode::Down) {
+            boundary *= 1.1;
+        }
+
         for x in 0..w {
             for y in 0..h {
+                let (a, b) = (
+                    map_value(x as f32, 0., 750., -boundary, boundary),
+                    map_value(y as f32, 0., 750., -boundary, boundary),
+                );
+
+                let mut z = Complex::new(a, b);
+
+                z = z * z - 1.;
+
+                // let norm = z.norm();
+
+                let angle = map_value(z.arg(), -PI as f32, PI as f32, 0., 360.);
+
+                let (r1, g1, b1) = angle_to_rgb(angle);
+
                 let color = Color {
-                    r: map_value(x as f32, 0., w as f32, 0., 255.),
-                    // g: map_value(y as f32, 0., h as f32, 0., 255.),
-                    g: 122.5,
-                    b: map_value(y as f32, 0., h as f32, 0., 255.),
-                    a: 1.,
+                    r: r1,
+                    g: g1,
+                    b: b1,
+                    a: 0.8,
                 };
+
                 image.set_pixel(x as u32, y as u32, color);
             }
+        }
+
+        // Draw axes
+
+        for x in 0..w {
+            image.set_pixel(x as u32, (h / 2) as u32, BLACK);
+        }
+        for y in 0..h {
+            image.set_pixel((w / 2) as u32, y as u32, BLACK);
         }
 
         texture.update(&image);
