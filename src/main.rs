ff2a21383683596b8a93ea8654f10456a32f037f
Luca Dupont <67842853+luca-dupont@users.noreply.github.com>
2024-05-21 16:46:53 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use macroquad::{color::hsl_to_rgb, miniquad::window::screen_size, prelude::*};
 use num_complex::Complex;
 use std::f32::consts::PI;
 
-// ! Change functions at lines 112 and below
+// ! Change function at line 23 and 32
 
 // Constants
 const ZOOM_FACTOR: f32 = 1.1;
@@ -20,6 +20,25 @@ fn map_value(value: f32, from_min: f32, from_max: f32, to_min: f32, to_max: f32)
     normalized_value * (to_max - to_min) + to_min
 }
 
+fn f_of_z(z : Complex<f32>) -> Complex<f32> {
+    // fz = ((-1. / 2.) * (z * z)).exp();
+    let fz = z * z * z - 1.;
+    // fz = z / z.cos();
+    // fz = z.sin() - 0.5;
+
+    fz
+}
+
+fn riemann_zeta(z : Complex<f32>) -> Complex<f32> {
+    let mut fz = Complex::new(0., 0.);
+
+    // 50 first terms of the sum
+    for i in 1..50 {
+        fz += z.expf(i as f32).inv();
+    }
+
+    fz
+}
 #[macroquad::main("Complex function plotter")]
 async fn main() {
     // Initialize window
@@ -41,7 +60,6 @@ async fn main() {
         // Check for window resizing
         let (new_width, new_height) = screen_size();
         if new_width != width || new_height != height {
-            
             image = Image::gen_image_color(new_width as u16, new_height as u16, WHITE);
             texture = Texture2D::from_image(&image);
 
@@ -99,20 +117,7 @@ async fn main() {
 
                 let mut z = Complex::new(a, b);
 
-                // //* ↓ Implementation for Riemann Zeta function with the first 50 terms | *SLOW* ↓ */
-                // let mut z2 = Complex::new(0., 0.);
-
-                // for i in 1..50 {
-                //     z2 += z.expf(i as f32).inv();
-                // }
-
-                // z = z2;
-
-                // ! ↓↓ CHANGE FUNC HERE ↓↓
-                // z = ((-1. / 2.) * (z * z)).exp();
-                // z = z * z * z - 1.;
-                z = z / z.cos();
-                // z = z.sin() - 0.5;
+                z = f_of_z(z);
 
                 // Map angle from radians to degrees
                 let hue = map_value(z.arg(), -PI, PI, 0., 1.);
@@ -130,35 +135,47 @@ async fn main() {
             }
         }
 
-        // Draw axes
-        for x in 0..w {
-            let y_pos = map_value(
-                0.,
-                (-boundary) + y_offset,
-                boundary + y_offset,
-                0.,
-                h as f32,
-            );
-            if y_pos < h as f32 && y_pos > 0. {
-                image.set_pixel(x as u32, y_pos as u32, BLACK);
-            }
-        }
-        for y in 0..h {
-            let x_pos = map_value(
-                0.,
-                (-boundary) + x_offset,
-                boundary + x_offset,
-                0.,
-                w as f32,
-            );
-            if x_pos < w as f32 && x_pos > 0. {
-                image.set_pixel(x_pos as u32, y as u32, BLACK);
-            }
-        }
-
         texture.update(&image);
 
         draw_texture(&texture, 0., 0., WHITE);
+
+        // Draw axes
+        let x_axe_pos = map_value(
+            0.,
+            (-boundary) + x_offset,
+            boundary + x_offset,
+            0.,
+            w as f32,
+        );
+        let y_axe_pos = map_value(
+            0.,
+            (-boundary) + y_offset,
+            boundary + y_offset,
+            0.,
+            h as f32,
+        );
+        draw_line(x_axe_pos, 0., x_axe_pos, h as f32, 1., BLACK);
+        draw_line(0., y_axe_pos, w as f32, y_axe_pos, 1., BLACK);
+
+        // Get point value
+        let (mut mx, mut my) = mouse_position();
+
+        mx = map_value(mx, 0., w as f32, -boundary + x_offset, boundary + x_offset);
+        my = map_value(my, 0., h as f32, -boundary + y_offset, boundary + y_offset);
+
+        let mut z = Complex::new(mx,my);
+
+        z = f_of_z(z);
+
+        let (re, im) = (z.re, z.im);
+
+        let z_text = &format!("z = {mx:.3} + {my:.3}i");
+        let fz_text = &format!("f(z) = {re:.3} + {im:.3}i");
+
+
+        draw_text(z_text, 10., 30., 30., BLACK);
+        draw_text(fz_text, 10., 60., 30., BLACK);
+
 
         next_frame().await;
     }
